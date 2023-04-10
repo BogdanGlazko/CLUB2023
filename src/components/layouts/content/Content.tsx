6a36c8a575ca14d7c763fd33df5f39d4dc658a2e
Bogdan <bhlazko@gmail.com>
2023-04-10 23:46:36 +0200

    routes2

--- src/components/layouts/content/Content.tsx
+++ src/components/layouts/content/Content.tsx
@@ -1,89 +1,41 @@
-// import React, {useEffect} from "react";
-// import s from "./content.module.sass"
-// import Messages from "./messages/Messages";
-// import Friends from "./friends/Friends";
-// import Feed from "./feed/Feed";
-// import {Navigate, Route, Routes, useLocation} from "react-router-dom";
-// import MyProfile from "./myProfile/MyProfile";
-// import {useSelector} from "react-redux";
-// import Loader from "../../shared/additionalComponents/Loader";
-// import Chat from "./chat/Chat";
-// import {IAppInitialState} from "interfaces/appInterfaces/appInterfaces";
-// import Settings from "./settings/Settings";
-// import ScrollButton from "../../shared/additionalComponents/ScrollUp";
-// import Marketplace from "./marketplace/Marketplace";
-//
-//
-// function Content() {
-//     const isLoading = useSelector((state: IAppInitialState) => state.loading)
-//
-//
-//     return (
-//         isLoading ?
-//             <div className={s.loader}><Loader/></div> :
-//             <div className={s.contentWrapper}>
-//                 <div id="content" className={s.content}>
-//                     <div id="scroll"></div>
-//                     <ScrollButton/>
-//
-//                     <Routes>
-//                         <Route path="profile" element={<MyProfile/>}/>
-//                         <Route path="*" element={<Navigate to="/profile"/>}/>
-//                         <Route path="messages" element={<Messages/>}/>
-//                         <Route path="feed" element={<Feed/>}/>
-//                         <Route path="friends" element={<Friends/>}/>
-//                         <Route path="chat" element={<Chat/>}/>
-//                         <Route path="marketplace" element={<Marketplace/>}/>
-//                         <Route path="settings" element={<Settings/>}/>
-//                     </Routes>
-//                 </div>
-//             </div>
-//     );
-// }
-//
-// export default Content;
-
-import React, {useEffect, useState} from "react";
-import s from "./content.module.sass";
+import React, {useEffect} from "react";
+import s from "./content.module.sass"
 import Messages from "./messages/Messages";
 import Friends from "./friends/Friends";
 import Feed from "./feed/Feed";
-import {Navigate, Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom";
 import MyProfile from "./myProfile/MyProfile";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import Loader from "../../shared/additionalComponents/Loader";
 import Chat from "./chat/Chat";
-import { IAppInitialState } from "interfaces/appInterfaces/appInterfaces";
+import {IAppInitialState} from "interfaces/appInterfaces/appInterfaces";
 import Settings from "./settings/Settings";
 import ScrollButton from "../../shared/additionalComponents/ScrollUp";
 import Marketplace from "./marketplace/Marketplace";
 
+
 function Content() {
-    const isLoading = useSelector((state: IAppInitialState) => state.loading);
-
-
-
-    return isLoading ? (
-        <div className={s.loader}>
-            <Loader />
-        </div>
-    ) : (
-        <div className={s.contentWrapper}>
-            <div id="content" className={s.content}>
-                <div id="scroll"></div>
-                <ScrollButton />
-                <Routes >
-                    <Route path="profile" element={<MyProfile />} />
-                    <Route path="messages" element={<Messages />} />
-                    <Route path="feed" element={<Feed />} />
-                    <Route path="friends" element={<Friends />} />
-                    <Route path="chat" element={<Chat />} />
-                    <Route path="marketplace" element={<Marketplace />} />
-                    <Route path="settings" element={<Settings />} />
-                    <Route path="*" element={<Navigate to="/profile" />} />
-                </Routes>
+    const isLoading = useSelector((state: IAppInitialState) => state.loading)
+    const location = useLocation();
+    return (
+        isLoading ?
+            <div className={s.loader}><Loader/></div> :
+            <div className={s.contentWrapper}>
+                <div id="content" className={s.content}>
+                    <div id="scroll"></div>
+                    <ScrollButton/>
+                    <Routes location={location}>
+                        <Route path="profile" element={<MyProfile/>}/>
+                        <Route path="login" element={<Navigate to="/profile"/>}/>
+                        <Route path="messages" element={<Messages/>}/>
+                        <Route path="feed" element={<Feed/>}/>
+                        <Route path="friends" element={<Friends/>}/>
+                        <Route path="chat" element={<Chat/>}/>
+                        <Route path="marketplace" element={<Marketplace/>}/>
+                        <Route path="settings" element={<Settings/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
     );
 }
 
